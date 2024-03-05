e26304d53999b2031c27dab7dc5026ccd9df056d
jkr033 <151925806+jkr033@users.noreply.github.com>
2024-03-05 16:20:42 +0200

    Add files via upload

---
+++ ketterät ryhmätyö PHP.php
@@ -0,0 +1,78 @@
+<?php
+
+    ini_set('display_errors',  '1');
+    ini_set('display_startup_errors',  '1');
+    error_reporting(E_ALL);
+
+
+$json = file_get_contents('ryhmätyö.json');
+$json_data = json_decode($json,true);
+if ($json_data === null) {
+    echo "Virhe JSONia";
+    exit;
+}
+
+$data = array();
+
+foreach ($json_data as $record) {
+    $person = array(
+'id' => $record['id'],
+'first_name'=> $record['first_name'],
+'last_name' => $record['last_name'],
+'email' => $record['email'],
+'gender' => $record['gender'],
+'ip_address' => $record['ip_address'],
+'car' => $record['car'],
+'hash' => $record['hash'],
+    );
+
+$data[] = $person;
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ryhmätyö</title>
+</head>
+<body>
+<h1>Asiakaslista</h1>
+ 
+<form id="form">
+    <input type="search" id="query" name="q"
+    placeholder="...">
+    <button>Etsi</button>
+</form>
+<table>
+     <tr>
+         <th>Id</th>
+         <th>First Name</th>
+         <th>Last Name</th>
+         <th>Email</th>
+         <th>Gender</th>
+         <th>IP Address</th>
+         <th>Car</th>
+         <th>Hash</th>
+ </tr>
+
+    <?php foreach ($data as $person): ?>
+        <tr>
+            <td><?php echo $person['id']; ?></td>
+            <td><?php echo $person['first_name']; ?></td>
+            <td><?php echo $person['last_name']; ?></td>
+            <td><?php echo $person['email']; ?></td>
+            <td><?php echo $person['gender']; ?></td>
+            <td><?php echo $person['ip_address']; ?></td>
+            <td><?php echo $person['car']; ?></td>
+            <td><?php echo $person['hash']; ?></td>
+    </tr>
+<?php endforeach; ?>
+    </table>
+    </body>
+    </html>
+
+
+
